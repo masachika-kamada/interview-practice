a5ea20ea5f4b25521de4f4a64cfc52b3e3b62858
{大城辰徳} <{tatsunori121soccer@icloud.com}>
2022-07-16 13:51:16 +0900

    質問音声の追加再生ボタン

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
+import streamlit.components.v1 as stc
+import base64
+import time
+
 
 class VideoProcessor:
     def __init__(self):
@@ -24,6 +28,25 @@ def main():
         # }
     )
 
+    question_button = st.button('アプリ実行')
+    if question_button:
+        audio_path1 = './question/1intoro.mp3' #入力する音声ファイル
+        audio_placeholder = st.empty()
+
+        file_ = open(audio_path1, "rb")
+        contents = file_.read()
+        file_.close()
+        audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
+        audio_html = """
+                        <audio autoplay=True>
+                        <source src="%s" type="audio/ogg" autoplay=True>
+                        Your browser does not support the audio element.
+                        </audio>
+                    """ %audio_str
+
+        audio_placeholder.empty()
+        time.sleep(0.5)
+        audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
