49eb9afa7c0712c9afa0b4da5bea884f72124f69
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-07-16 16:00:10 +0900

    画面遷移の設計

--- app.py
+++ app.py
@@ -14,21 +14,37 @@ class VideoProcessor:
 
 
 def main():
-    st.markdown('# 面接練習')
 
+    st.session_state["question"]=0
+
+    st.markdown('# Interview-Practice')
+
+    st.button('質問１', on_click=question1)
+
+    st.button('質問２', on_click=question2)
+
+    st.button('質問３', on_click=question3)
+
+    st.button('質問４', on_click=question4)
+
+    st.button('質問５', on_click=question5)
+
+    st.markdown(st.session_state["question"])
+
+def record_page():
+    st.markdown(st.session_state["question"])
     webrtc_streamer(
-        key="",
-        video_processor_factory=VideoProcessor,
-        # デプロイ時にコメントアウト除去
-        # rtc_configuration={
-        #     "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-        # }
+    key="",
+    video_processor_factory=VideoProcessor,
+    # デプロイ時にコメントアウト除去
+    # rtc_configuration={
+    #     "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    # }
     )
 
     # 値の受け渡し
-    st.session_state["results"]='hoge'
     st.button("結果へ", on_click=show_result)
-
+    st.button("ホームへ", on_click=show_home)
 
 def result_page(): 
     # css作成
@@ -47,7 +63,7 @@ def result_page():
 
     st.title("結果")
     # 値受け取り
-    st.markdown(st.session_state["results"])
+    # st.markdown(st.session_state["results"])
 
     st.button('ホームへ', on_click=show_home)
     
@@ -55,14 +71,42 @@ def show_home():
     # homeへ切り替え
     st.session_state["page_control"]=0
 
+def question1():
+    # homeへ切り替え
+    st.session_state["page_control"]=1
+    st.session_state["question"]=1
+
+def question2():
+    # homeへ切り替え
+    st.session_state["page_control"]=2
+    st.session_state["question"]=2
+
+def question3():
+    # homeへ切り替え
+    st.session_state["page_control"]=3
+    st.session_state["question"]=3
+
+def question4():
+    # homeへ切り替え
+    st.session_state["page_control"]=4
+    st.session_state["question"]=4
+
+def question5():
+    # homeへ切り替え
+    st.session_state["page_control"]=5
+    st.session_state["question"]=5
+
 def show_result():
     # resultへ切り替え
-    st.session_state["page_control"]=1
+    st.session_state["page_control"]=6
 
 # 状態保持する変数を作成して確認
 if ("page_control" in st.session_state and
-    st.session_state["page_control"] == 1):
+    st.session_state["page_control"] > 0 and 
+    st.session_state["page_control"] < 6 ):
+    record_page()
+elif ("page_control" in st.session_state and
+    st.session_state["page_control"] == 6):
     result_page()
 else:
-    st.session_state["page_control"] = 0
     main()
