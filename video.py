a39708ce5f5fb4c853e349f29b43a9247f089f82
masachika <masachika.kmd@gmail.com>
2022-07-16 21:36:40 +0900

    [update] #1 視線検出を実装

--- video.py
+++ video.py
@@ -9,10 +9,12 @@ class VideoProcessor:
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
         self.margin = 5
+        self.thresh_diff_eyes_hor = 0.2
+        self.thresh_diff_eyes_ver = 0.1
 
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
-        ret, pupil_coords = self.__detect_eyes(img)
+        ret, pupil_coords, eye_tracks = self.__detect_eyes(img)
 
         if ret is False:
             return frame
@@ -21,6 +23,24 @@ class VideoProcessor:
             if coord is None:
                 continue
             cv2.drawMarker(img, coord, (0, 0, 255), cv2.MARKER_CROSS, markerSize=10, thickness=2)
+            eye_hor_l = 1 - eye_tracks[0][0]
+            eye_hor_r = eye_tracks[1][0]
+            diff_eyes_hor = eye_hor_l - eye_hor_r
+            if abs(diff_eyes_hor) < self.thresh_diff_eyes_hor:
+                cv2.putText(img, "Center", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            elif diff_eyes_hor < 0:
+                cv2.putText(img, "Left", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            else:
+                cv2.putText(img, "Right", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # eye_ver_l = eye_tracks[0][1]
+            # eye_ver_r = eye_tracks[1][1]
+            # diff_eyes_ver = (eye_ver_l + eye_ver_r) / 2
+            # if abs(diff_eyes_ver - 0.5) < self.thresh_diff_eyes_ver:
+            #     cv2.putText(img, "Center", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # elif diff_eyes_ver < 0.5:
+            #     cv2.putText(img, "Up", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # else:
+            #     cv2.putText(img, "Down", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
     def on_ended(self):
@@ -31,7 +51,7 @@ class VideoProcessor:
 
         faces = self.detector(img_g)
         if len(faces) == 0:
-            return False, None
+            return False, None, None
 
         for face in faces:
             landmarks = self.predictor(img_g, face)
@@ -39,31 +59,36 @@ class VideoProcessor:
         # 右目：[36, 37, 39, 40] 左目：[42, 43, 45, 46]
         eyes_idx = [[36, 37, 39, 40], [42, 43, 45, 46]]
         pupil_coords = []
+        eye_track = []
         for idx in eyes_idx:
-            ref_point, eye_img = self.__get_eye_img(img, landmarks, idx)
-            coord = self.__get_pupil_coord(ref_point, eye_img)
-            pupil_coords.append(coord)
-        return True, pupil_coords
+            ref_point, eye_img, w, h = self.__get_eye_img(img, landmarks, idx)
+            cx, cy = self.__get_pupil_coord(eye_img)
+            pupil_coords.append((cx + ref_point[0], cy + ref_point[1]))
+            eye_track.append((cx / w, cy / h))
+        return True, pupil_coords, eye_track
 
     def __get_eye_img(self, img, landmarks, idx):
         xmin = landmarks.part(idx[0]).x
-        ymin = landmarks.part(idx[1]).y
+        ymin = landmarks.part(idx[1]).y - self.margin
         xmax = landmarks.part(idx[2]).x
-        ymax = landmarks.part(idx[3]).y
-        return (xmin, ymin), img[ymin - self.margin:ymax + self.margin, xmin:xmax]
+        ymax = landmarks.part(idx[3]).y + self.margin
+        return (xmin, ymin), img[ymin:ymax, xmin:xmax], xmax - xmin, ymax - ymin
 
-    def __get_pupil_coord(self, ref_point, img):
+    def __get_pupil_coord(self, img):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         Hl, Sl, Vl = 0, 0, 11
         Hu, Su, Vu = 179, 255, 77
         lower = np.array([Hl, Sl, Vl])
         upper = np.array([Hu, Su, Vu])
         mask = cv2.inRange(hsv, lower, upper)
-        cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        try:
+            cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        except Exception as e:
+            print(e)
+            return None, None
         if len(cnts) == 0:
-            return None
+            return None, None
         cnt = max(cnts, key=lambda x: cv2.contourArea(x))
 
         x, y, w, h = cv2.boundingRect(cnt)
-        x, y, w, h = x + ref_point[0], y + ref_point[1], w, h
-        return (int(x + w / 2), int(y + h / 2))
+        return int(x + w / 2), int(y + h / 2)
