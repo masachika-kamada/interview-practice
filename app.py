e39c98841f26473bec9838c3a9352c73f94d3a31
河野竜也 <kawanotatsuya@kounotatsuyanoMacBook-Pro.local>
2022-07-16 14:51:38 +0900

    結果画面

--- app.py
+++ app.py
@@ -26,7 +26,13 @@ def main():
     )
 
     # 値の受け渡し
-    st.session_state["results"]='hoge'
+    st.session_state['camera']='70'
+    st.session_state['smile']='30'
+    st.session_state['normal']='40'
+    st.session_state['interviwer']='良'
+    st.session_state['volume']='ちょうど良い'
+    st.session_state['silence']='50'
+    st.session_state['rank']='B'
     st.button("結果へ", on_click=show_result)
 
 
@@ -40,17 +46,29 @@ def result_page():
         border-radius: 10px 10px 10px 10px ;
         background   : #ddd                ;
     }}
+    div.main_container {{
+        ext-align: center;
+    }}
     </style>
     """
     # css適用
     st.markdown(result_css, unsafe_allow_html=True)
-
-    st.title("結果")
+    main_container = st.container()
+    main_container.title("分析結果")
+    main_container.markdown("自己紹介をお願いします")
+    # 録画表示 
+    stc.html("<iframe width=’560’ height=’315’ src='https://www.youtube.com/embed/3QPp_DlcZpM' title=’YouTube video player’ frameborder=’0’ allow=’accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture’ allowfullscreen></iframe>")
     # 値受け取り
-    st.markdown(st.session_state["results"])
+    container = st.container()
+    container.markdown("カメラ目線" + st.session_state['camera'])
+    container.markdown("笑顔：" + st.session_state['smile'] + '秒 / 普通' + st.session_state['normal'] + '秒')
+    container.markdown("面接官からの印象" + st.session_state['interviwer'])
+    container.markdown("声の大きさ" + st.session_state['volume'])
+    container.markdown("無言の時間率" + st.session_state['silence'])
+    container.markdown("面接基礎力ランク" + st.session_state['rank'])
 
-    st.button('ホームへ', on_click=show_home)
-    
+    st.button('他の質問で分析してみる', on_click=show_home)
+
 def show_home():
     # homeへ切り替え
     st.session_state["page_control"]=0
