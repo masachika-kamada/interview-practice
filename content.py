904f8a1abbfbc99bfa4b47381016fabaf71acde7
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-09-09 01:25:32 +0900

    新しい画像の挿入、スコア及び画像表示のロジック完成

--- content.py
+++ content.py
@@ -135,6 +135,8 @@ def result_upper(idx):
         </style>
         """
     return content
+#global変数（スコアによって画像を変更するための初期値）
+score_img_number = 1
 
 def result_lower():
     with open('./results/eye_track.json') as f:
@@ -143,30 +145,43 @@ def result_lower():
     with open('./results/voice_analyze.json') as f:
         voice_analyze = json.load(f)
 
-    randomi = random.randrange(0, 4)
+    # 分かりやすいように変数定義
+    eye = round(eye_track['eye_center_ratio']*100)
+    mic_off = round(voice_analyze['mic_off_ratio']*100)
+    smile = 50
+    volume = 50
+
     img_paths = [
         "https://user-images.githubusercontent.com/63488322/188748819-3b49bf32-a860-4864-b5e2-ce7db8f214fb.jpg",
         "https://user-images.githubusercontent.com/63488322/188748809-06f71abc-31e9-4b93-92a2-e634d49e260d.jpg",
         "https://user-images.githubusercontent.com/63488322/188748816-f25bed0c-8cb6-4772-9032-a8fce86168c8.jpg",
-        "https://user-images.githubusercontent.com/63488322/188748818-c5598617-1b19-4b91-b637-fd4883382cef.jpg"
+        "https://user-images.githubusercontent.com/63488322/188748818-c5598617-1b19-4b91-b637-fd4883382cef.jpg",
+        "https://user-images.githubusercontent.com/84577532/189152326-fbeea9e3-5e3c-4769-b7d9-f846bbeab3f3.jpg",
+        "https://user-images.githubusercontent.com/84577532/189152724-fece6f81-a266-4fc5-a1f5-f5ace0f61180.jpg",
+        "https://user-images.githubusercontent.com/84577532/189152935-5c1e7183-4946-41de-bb69-8bb57887e233.jpg",
+        "https://user-images.githubusercontent.com/84577532/189158919-f638cbb8-37ba-4cdc-a5bf-2a22bec1a82a.jpg",
+        "https://user-images.githubusercontent.com/84577532/189159583-1b009fe1-f41f-4ec4-afc0-d48814674d0c.jpg",
+        "https://user-images.githubusercontent.com/84577532/189159935-183b960c-d22c-4414-8739-e785ae2d12f9.jpg",
+        "https://user-images.githubusercontent.com/84577532/189159949-47859b08-80a7-440a-bebc-b11d33f03779.jpg"
+
     ]
 
     content = """
     <div class='wrapper'>
         <div class='first'>
-            <p>カメラ目線率<span class='camera_coma'>：</span><span class='point camera_span'>""" + str(round(eye_track['eye_center_ratio']*100)) + """</span>%</p>
-            <p>無言の時間率<span class='mute_coma'>：</span><span class='point mute_span'>""" + str(round(voice_analyze['mic_off_ratio']*100)) + """</span>%</p>
+            <p>カメラ目線率<span class='camera_coma'>：</span><span class='point camera_span'>""" + str(eye) + """</span>%</p>
+            <p>無言の時間率<span class='mute_coma'>：</span><span class='point mute_span'>""" + str(mic_off) + """</span>%</p>
         </div>
         <div class='second'>
-            <p>笑顔率<span class='smile_coma'>：</span><span class='point smile_span'>""" + str(round(eye_track['face_smile_ratio']*100)) +  """</span>%</p>
-            <p>声量<span class='volume_coma'>：</span><span class='point volume_span'>""" + "volume" +  """</span></p>
+            <p>笑顔率<span class='smile_coma'>：</span><span class='point smile_span'>""" + str(smile) +  """</span>%</p>
+            <p>声量<span class='volume_coma'>：</span><span class='point volume_span'>""" + str(volume) +  """</span></p>
         </div>
     </div>
     <div class='under_wrapper'>
-        <p>面接官からの印象<span class='impression_coma'>：</span><span class='point impression_span'>""" + "calc_score" + """</span></p>
+        <p>面接官からの印象<span class='impression_coma'>：</span><span class='point impression_span'>""" + calc_impression(eye, smile, mic_off, volume)  + """</span></p>
     </div>
     <div class="img_div">
-        <img src=""" + img_paths[randomi] + """>
+        <img src=""" + img_paths[score_img_number] + """>
     </div>
     <style>
     .wrapper {
@@ -211,17 +226,25 @@ def result_lower():
     </style>
     """
     return content
+    
 
-def calc_score(eye, smile, volume, mic_off):
-    score = eye + smile - mic_off
+
+def calc_impression(eye, smile, mic_off, volume):
+    score = eye + smile - mic_off + volume - 100
     # 全部均一に換算してるが、優先順位とか考えてもいいかも
     # volumeを計算式に追加する必要あり
+    global score_img_number
 
     if score >= 300:
-        return "good"
+        # img_divでの画像表示用の番号を定義
+        score_img_number = random.randrange(0, 4)
+        return "大変すばらしい面接です。この調子で頑張ってください！"
     elif score >= 200:
-        return "normal"
+        score_img_number = random.randrange(4, 7)
+        return "おおむねよくできています！"
     elif score >= 100:
-        return "not good"
+        score_img_number = random.randrange(7, 9)
+        return "もう少し頑張りましょう"
     else:
-        return "Bad"
+        score_img_number = random.randrange(9, 11)
+        return "めんたつで再度練習しましょう"
